f359eb63ece583004366819461cb46e14ad4d502
saurabh9801 <95734900+saurabh9801@users.noreply.github.com>
2025-04-14 04:38:37 +0000

    CODE COMMIT

---
+++ 04_comparision-opt.js
@@ -0,0 +1,12 @@
+// console.log(null > 0); //out:false
+// console.log(null == 0) // out: false
+// console.log(null >= 0) // true
+
+// console.log(undefined == 0) //false
+// console.log(undefined > 0)//false
+// console.log(undefined < 0)//false
+
+
+
+// === it will check the value and datatypes also 
+// console.log("2" === 2)
