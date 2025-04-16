24e245b5ea72c9204e0cad39d46131fb13875f3c
saurabh9801 <95734900+saurabh9801@users.noreply.github.com>
2025-04-16 17:47:14 +0000

    code commit

--- 08_array.js
+++ 08_array.js
@@ -25,4 +25,48 @@ const newArray = myArray.join()
 
 
 // slice & splice
-console.log("A", myArray)
+// console.log("A", myArray)
+
+
+
+// part Array
+
+
+const marvel_heros  = ["thor","Ironman","spiderman"];
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);oout:[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ] ,  Push method push the data in the same array 
+// in this dc_heros it is considered as a element
+// console.log(marvel_heros[3][1])if we want to access the value we have to do like this out:flash
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); here it will create a new array using the concat method
+
+
+
+// spread operator: it will spread the operator 
+const spreadHeros = [...marvel_heros,...dc_heros]
+// console.log(spreadHeros); here it will merge the array out:[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+// const another_array  = [1,2,3,[2,6,7],4,[4,8,7,[0,5]]];
+// const real_another_array = another_array.flat(Infinity)
+// console.log(another_array);
+
+
+//how to convert string into array 
+// console.log(Array.isArray("saurabh")); out:false
+// console.log(Array.from("saurabh")) by using from we can convert the string into array
+// console.log(Array.from({name:"saurabh"})); here it will give the [ ]array
+
+
+// how to add so many varibale into one array
+
+let socre1 = 100;
+let score2 = 200;
+let score3 = 300;
+// console.log(Array.of(socre1,score2,score3)); out:[100,200,300]
+
+
+
+
---
+++ 09_objects.js
@@ -0,0 +1,50 @@
+//singelton
+//object literals
+
+const mySymbol = ("Key1")
+const JsUser = {
+    name: "saurabh",
+    "full name":"saurabh kumar",
+    [mySymbol] :"myKey1",
+    age: 25,
+    location: "bangalore",
+    email : "sk@gmail.in",
+    isLoggedIn : false,
+    lastLoginDays :["Monday","Tuesady"]
+
+}
+// console.log(JsUser.email);
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySymbol])
+
+
+// how to chnage the value of the object
+
+
+JsUser.email = "skp@gmail.com";
+// console.log(JsUser.email);
+
+
+// How to freeze the object vlaue 
+// Object.freeze(JsUser)
+// JsUser.email = "dsdsds@gmail.com"
+// console.log(JsUser)
+
+
+//function
+JsUser.greeting = function(){
+    // console.log("hi saurabh");
+    
+}
+// console.log(JsUser.greeting) out:[Function (anonymous)]
+// console.log(JsUser.greeting()) out:hi saurabh
+// undefined
+
+
+// how to take the refernce of the object in function
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user, ${this.name}`);//here this take the refrence of the object and what are the property in the Jsuser object
+    
+}
+console.log(JsUser.greetingTwo())
